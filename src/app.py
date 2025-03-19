89700113722b3c1379d58e08bfa48bb545963dd7
Miguel Toyas Pernichi <mitoperni@mac.home>
2025-03-19 10:25:46 +0000

    Metodos agregados

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Post
 #from models import Person
 
 app = Flask(__name__)
@@ -56,7 +56,7 @@ def get_user(user_id):
 
     if not user:
         return jsonify({"msg": "User not found"}), 404
-    
+
     return jsonify({
         "msg": "User found succesfully",
         "user" : user.serialize_is_active()
@@ -72,12 +72,12 @@ def create_user():
     # Comprobamos que los campos no estén vacíos
     if not request_data.get('email') or not request_data.get('password'):
         return jsonify({"msg": "Email and password are required"}), 400
-    
+
     # Comprobamos que el email de este usuario no existe ya:
     existing_user = User.query.filter_by(email = request_data.get('email')).first()
     if existing_user:
         return jsonify({"msg": "User already exists"}), 403
-    
+
     # Creamos ese nuevo usuario
     new_user = User(
         email = request_data["email"],
@@ -94,6 +94,60 @@ def create_user():
         "msg" : "User created succesfully",
         "user" : new_user.serialize()
     }), 201
+
+@app.route('/user/posts/<int:user_id>', methods=['GET'])
+def get_user_posts(user_id):
+
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"msg":"No se encontro el usuario"}), 404
+
+    return jsonify({
+        "msg": "User found succesfully",
+        "user" : user.serialize_posts()
+    }), 200
+
+@app.route('/user/post', methods=['POST'])
+def create_post():
+
+    request_data = request.get_json()
+
+    if not request_data or not request_data.get("title") or not request_data.get("content") or not request_data.get("user_id"):
+        return jsonify({"msg" : "Petición incompleta"}), 400
+
+    user = User.query.get(request_data.get("user_id"))
+
+    if not user:
+        return jsonify({"msg" : "El usuario no existe"}), 404
+
+    new_post = Post(
+        title = request_data.get("title"),
+        content = request_data.get("content"),
+        user_id = request_data.get("user_id")
+    )
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return jsonify({
+        "msg" : "Post creado con exito",
+        "post" : new_post.serialize()
+    }), 201
+
+@app.route('user/post/<int:post_id>', methods = ['DELETE'])
+def delete_post(post_id):
+
+    post = Post.query.get(post_id)
+
+    if not post:
+        return jsonify({"msg" : "Post no encontrado"}), 404
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return jsonify({"msg": "Post borrado con éxito"}), 200
+
 
 
 
--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -10,6 +10,8 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    # Relación entre tablas:
+    posts = relationship("Post", back_populates = "user", cascade = "all, delete-orphan")
 
     def serialize(self):
         return {
@@ -17,11 +19,42 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
-    
+
     def serialize_is_active(self):
         return {
             "id": self.id,
             "email": self.email,
             "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+
+    def serialize_posts(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "posts": [post.serialize_all() for post in self.posts]
+        }
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200), nullable = False)
+    content: Mapped[str] = mapped_column(String(500), nullable = False)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"), nullable = False)
+
+    # Relación
+    user = relationship("User", back_populates="posts")
+
+    def serialize_all(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content
+        }
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "user_id": self.user_id
         }
